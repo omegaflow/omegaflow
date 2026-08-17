bf8301746a1f2808cda15e5b426fbac70968646c
omegaflow <code@omegaflow.space>
2026-08-18 01:19:46 +0200

    chunk_master: alle 4 großen Kataloge (pastel/wds/mktypes/denis) für pm-Recompile

--- phi/port/chunk_master.py
+++ phi/port/chunk_master.py
@@ -67,6 +67,12 @@ def chunk_catalog(name, table, columns, skip_null, ra_col, bands):
             os.remove(p)
 
 if __name__ == "__main__":
+    chunk_catalog("pastel", "B/pastel/pastel",
+                  "ra:RAdeg;dec:DEdeg;teff:Teff;logg:logg;mag:Vmag", "teff", "RAdeg",
+                  [(lo, lo+45) for lo in range(0, 360, 45)])
+    chunk_catalog("wds", "B/wds/wds",
+                  "ra:RAJ2000;dec:DEJ2000;mag1:mag1;mag2:mag2;sep:sep1", "mag1", "RAJ2000",
+                  [(lo, lo+45) for lo in range(0, 360, 45)])
     chunk_catalog("mktypes", "B/mk/mktypes",
                   "ra:RAJ2000;dec:DEJ2000;mag:Mag", "mag", "RAJ2000",
                   [(lo, lo+4.5) for lo in [x*4.5 for x in range(80)]])
